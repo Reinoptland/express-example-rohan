7fc7d2d3eb9bf5b5123b81f92c81c0f119bd5adb
Reinoptland <reinoptland@gmail.com>
2022-10-20 10:18:44 +0200

    Add GET, POST /students + express.json middleware

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const express = require("express");
 const app = express();
 
+// read request bodies when they are Content-Type: application/json
+app.use(express.json());
+
 const PORT = process.env.PORT || 4000;
+
+const students = [
+  { id: 1, name: "Rohan" },
+  { id: 2, name: "Alina" },
+  { id: 3, name: "Pierre" },
+];
 
 app.get("/", (req, res) => {
   res.send("HELLO");
+});
+
+app.get("/students", (req, res) => {
+  res.json(students);
+});
+
+app.post("/students", (req, res) => {
+  //   console.log("ARE WE GETTING A POST REQUEST??", req.body);
+  const newStudent = { id: students.length + 1, name: req.body.name };
+  students.push(newStudent);
+  res.status(201).json({ message: "Student created" });
 });
 
 app.listen(PORT, () => {
