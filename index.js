c36424b38f2a772d0e4f91a18006b256c6769638
Reinoptland <reinoptland@gmail.com>
2022-10-24 09:56:16 +0200

    DELETE /students/:id

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(express.json()); // "use"ing a middleware
 
 const PORT = process.env.PORT || 4000;
 
-const students = [
+let students = [
   { id: 1, name: "Rohan" },
   { id: 2, name: "Alina" },
   { id: 3, name: "Pierre" },
@@ -25,9 +25,7 @@ app.get("/students", (req, res) => {
 // GET /students/:id
 // route parameter -> :id
 app.get("/students/:id", (req, res) => {
-  const student = students.find(
-    (student) => student.id === Number(req.params.id)
-  );
+  const student = findStudentById(Number(req.params.id));
 
   if (!student) {
     return res.status(404).json({ message: "student not found" });
@@ -43,6 +41,23 @@ app.post("/students", (req, res) => {
   res.status(201).json({ message: "Student created" });
 });
 
+// DELETE /students/:id
+app.delete("/students/:id", (req, res) => {
+  const student = findStudentById(Number(req.params.id));
+
+  if (!student) {
+    return res.status(404).json({ message: "student not found" });
+  }
+
+  students = students.filter((student) => student.id !== Number(req.params.id));
+
+  res.json({ message: "student deleted" });
+});
+
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}`);
 });
+
+function findStudentById(studentId) {
+  return students.find((student) => student.id === studentId);
+}
