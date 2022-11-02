9c03ff0049b32682d475be23fdd25c0b31b49a68
Reinoptland <reinoptland@gmail.com>
2022-11-02 13:08:39 +0100

    Rewrite express endpoints

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
-const { response } = require("express");
 const express = require("express");
 const app = express();
 const z = require("zod");
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
 // read request bodies when they are Content-Type: application/json
 app.use(express.json()); // "use"ing a middleware
 
 const PORT = process.env.PORT || 4000;
-
-let students = [
-  { id: 1, name: "Rohan", city: "Uithoorn" },
-  { id: 2, name: "Alina", city: "Almere" },
-  { id: 3, name: "Pierre", city: "Amsterdam" },
-];
 
 // GET /
 app.get("/", (req, res) => {
@@ -21,47 +16,49 @@ app.get("/", (req, res) => {
 
 // READ
 // GET /students
-app.get("/students", (req, res) => {
+app.get("/students", async (req, res) => {
   // get from db
+  const students = await prisma.student.findMany();
   res.json(students);
 });
 
 // READ
 // GET /students/:id
 // route parameter -> :id
-app.get("/students/:id", (req, res) => {
-  const student = findStudentById(Number(req.params.id));
+app.get("/students/:id", async (req, res) => {
+  try {
+    const student = await prisma.student.findUniqueOrThrow({
+      where: { id: Number(req.params.id) },
+    });
 
-  if (!student) {
-    return res.status(404).json({ message: "student not found" });
+    res.json(student);
+  } catch (error) {
+    if (error.name === "NotFoundError") {
+      return res.status(404).json({ message: "student not found" });
+    } else {
+      console.log(error);
+      return res.status(500).json({ message: "something went wrong" });
+    }
   }
-
-  res.json(student);
 });
 
 // CREATE
 // POST /students
 
-const Student = z.object({
-  name: z.string().min(2),
-  city: z.string().min(2),
-  email: z.string().email(),
-});
+const StudentSchema = z
+  .object({
+    name: z.string().min(2),
+    city: z.string().min(2),
+  })
+  .strict();
 
-app.post("/students", (req, res) => {
+app.post("/students", async (req, res) => {
   try {
-    const validatedInput = Student.parse(req.body);
-    console.log(validatedInput);
+    const validatedInput = StudentSchema.parse(req.body);
 
-    const newStudent = {
-      id: students.length + 1,
-      name: req.body.name,
-      city: req.body.city,
-    };
-    students.push(newStudent);
-    res.status(201).json({ message: "Student created" });
+    const newStudent = await prisma.student.create({ data: validatedInput });
+    res.status(201).json({ message: "Student created", student: newStudent });
   } catch (error) {
-    console.log(error.issues, error.name);
     if (error.name === "ZodError") {
       return res
         .status(400)
@@ -74,40 +71,48 @@ app.post("/students", (req, res) => {
 
 // DESTROY
 // DELETE /students/:id
-app.delete("/students/:id", (req, res) => {
-  const student = findStudentById(Number(req.params.id));
-
-  if (!student) {
-    return res.status(404).json({ message: "student not found" });
+app.delete("/students/:id", async (req, res) => {
+  try {
+    const student = await prisma.student.delete({
+      where: { id: Number(req.params.id) },
+    });
+    res.json({ message: "student deleted" });
+  } catch (error) {
+    if (error.code === "P2025") {
+      return res.status(404).json({ message: "student not found" });
+    } else {
+      console.log(error);
+      return res.status(500).json({ message: "Something went wrong" });
+    }
   }
-
-  students = students.filter((student) => student.id !== Number(req.params.id));
-
-  res.json({ message: "student deleted" });
 });
 
-app.patch("/students/:id", (req, res) => {
-  const student = findStudentById(Number(req.params.id));
+app.patch("/students/:id", async (req, res) => {
+  try {
+    const validatedInput = StudentSchema.parse(req.body);
 
-  if (!student) {
-    return res.status(404).json({ message: "student not found" });
+    const student = await prisma.student.update({
+      where: {
+        id: Number(req.params.id),
+      },
+      data: validatedInput,
+    });
+
+    res.json(student);
+  } catch (error) {
+    if (error.name === "ZodError") {
+      return res
+        .status(400)
+        .json({ message: "validation error", errors: error.issues });
+    } else if (error.code === "P2025") {
+      return res.status(404).json({ message: "student not found" });
+    } else {
+      console.log("UNEXPECTED!", error);
+      return res.status(500).json({ message: "Something went wrong, sorry!" });
+    }
   }
-
-  // console.log("CURRENT STUDENT:", student, "DATA FROM THE REQUEST", req.body);
-  // update student -> current student, data from the request
-  // combine the 2
-  // student.name = req.body.name;
-  for (key in req.body) {
-    student[key] = req.body[key];
-  }
-
-  res.json(student);
 });
 
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}`);
 });
-
-function findStudentById(studentId) {
-  return students.find((student) => student.id === studentId);
-}
